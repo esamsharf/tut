03640a31d8a8a73ac9eeb9990e6f352c7fc15cca
“esamsharf” <esamsharf2@gmail.com>
2023-04-20 01:35:10 +0200

    Add the routes of the App

---
+++ lib/presentation/routesManager.dart
@@ -0,0 +1,23 @@
+class Routes {
+  static const String splashRoute = '/';
+  static const String loginRoute = '/login';
+  static const String homeRoute = '/home';
+  static const String profileRoute = '/profile';
+  static const String settingsRoute = '/settings';
+  static const String aboutRoute = '/about';
+  static const String contactRoute = '/contact';
+  static const String onBoardingRoute = '/onBoarding';
+  static const String registerRoute = '/register';
+  static const String forgotPasswordRoute = '/forgotPassword';
+  static const String resetPasswordRoute = '/resetPassword';
+  static const String changePasswordRoute = '/changePassword';
+  static const String changeEmailRoute = '/changeEmail';
+  static const String changePhoneRoute = '/changePhone';
+  static const String changeNameRoute = '/changeName';
+  static const String changeAddressRoute = '/changeAddress';
+  static const String changeGenderRoute = '/changeGender';
+  static const String changeBirthDateRoute = '/changeBirthDate';
+  static const String changeLanguageRoute = '/changeLanguage';
+  static const String mainRoute = '/main';
+  static const String storeDetailsRoute = '/storeDetails';
+}
---
+++ lib/presentation/storeDetais/storeDetais.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StoreDetaisView extends StatefulWidget {
+  const StoreDetaisView({Key? key}) : super(key: key);
+
+  @override
+  State<StoreDetaisView> createState() => _StoreDetaisViewState();
+}
+
+class _StoreDetaisViewState extends State<StoreDetaisView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
